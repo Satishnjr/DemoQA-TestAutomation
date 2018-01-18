f4e36cbf2ceb96eac6c49b443ad8cf52c13052ca
Satishnjr <sksatishreddy@gmail.com>
2018-01-18 13:37:53 +0530

    no message

--- src/test/java/step_definitions/TestRunner.java
+++ src/test/java/step_definitions/TestRunner.java
@@ -6,13 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-<<<<<<< HEAD
-@CucumberOptions(features = "classpath:features/RegistrationForm.feature", plugin = {
-		"pretty", "html:target/cucumber-html-report", "json:cucumber.json" }, tags = {})
-=======
 @CucumberOptions(features = "classpath:features", plugin = { "pretty",
 		"html:target/cucumber-html-report", "json:cucumber.json" }, tags = {})
->>>>>>> 0b017ef029d72c18bcc2acf9d9c86da7121d2f62
 public class TestRunner {
 
 }
